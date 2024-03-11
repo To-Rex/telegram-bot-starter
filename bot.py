816960858d9c596ec1d1bc9a5b8084968474f580
to-rex <torex.amaki@gmail.com>
2024-03-11 22:35:41 +0500

    first

--- bot.py
+++ bot.py
@@ -54,11 +54,10 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 def main() -> None:
-    """Start the bot."""
     load_dotenv()
 
     # Create the Application and pass it your bot's token.
-    application = Application.builder().token(os.environ["TOKEN"]).build()
+    application = Application.builder().token("6442396283:AAF0b_xjCe2404I-chwZaHptXh6dxtCnKpI").build()
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
